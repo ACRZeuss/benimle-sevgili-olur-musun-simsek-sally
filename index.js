2772aa5273ebac7613f72dcdae1557744462733f
Erhan <ep4963807@gmail.com>
2024-04-23 21:41:18 +0300

    update

---
+++ index.js
@@ -0,0 +1,17 @@
+function moveButton() {
+    var button = document.getElementById("button2");
+    var buttonWidth = button.offsetWidth;
+    var buttonHeight = button.offsetHeight;
+    var containerWidth = 1112;
+    var containerHeight = 250;
+
+    // Butonun rastgele x ve y konumları hesaplanır
+    var maxX = containerWidth - buttonWidth;
+    var maxY = containerHeight - buttonHeight;
+    var newX = Math.floor(Math.random() * maxX);
+    var newY = Math.floor(Math.random() * maxY);
+
+    // Butonun pozisyonu ayarlanır
+    button.style.left = newX + "px";
+    button.style.top = newY + "px";
+  }
